43b94c2dac06513c7ca1429761e128bef1bbc8cb
yejian <285913930@qq.com>
2018-12-13 11:21:51 +0800

    1

--- lib/common/HttpHelper.php
+++ lib/common/HttpHelper.php
@@ -32,48 +32,5 @@ class HttpHelper
                 && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
             ? 'https:' : 'http:';
     }
-
-    public static function outputFile($filePath, $outputFileName = null)
-    {
-        if (empty($filePath))
-        {
-            return;
-        }
-
-        $fileName = empty($outputFileName) ? basename($filePath) : $outputFileName;
-        $encodedFilename = str_replace("+", "%20", urlencode($fileName));
-
-        $userAgent = $_SERVER["HTTP_USER_AGENT"];
-
-        header('Content-Length: ' . filesize($filePath));
-        header('Content-type:' . mime_content_type($filePath));
-        header("Content-Transfer-Encoding: binary");
-        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-        header('Pragma: public'); // HTTP/1.0
-        header("Expires: 0");
-        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-        header("Cache-Control: public");
-
-        if (preg_match("/MSIE/", $userAgent)
-            || preg_match("/Trident\/7.0/", $userAgent)
-            || preg_match("/Edge/", $userAgent))
-        {
-            header('Content-Disposition: attachment; filename="' . $encodedFilename . '"');
-        }
-        else
-        {
-            if (preg_match("/Firefox/", $userAgent))
-            {
-                header('Content-Disposition: attachment; filename*="utf8\'\'' . $fileName . '"');
-            }
-            else
-            {
-                header('Content-Disposition: attachment; filename="' . $fileName . '"');
-            }
-        }
-
-        readfile($filePath);
-    }
-
 }
 
